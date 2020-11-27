e1856d03b740d12606515b4cada2bb332ca2783e
Levente3D <faylevente@gmail.com>
2020-11-27 23:05:46 +0100

    Week05d02 commit

---
+++ src/test/java/week05d02/ChangeLetterTest.java
@@ -0,0 +1,14 @@
+package week05d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class  ChangeLetterTest {
+
+    @Test
+    void testChangeLetters(){
+
+        assertEquals("*rv*zt*r*", new ChangeLetter().changeVowels("arvizturo"));
+    }
+}
