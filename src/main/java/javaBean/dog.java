55da7411850dbd47e0ba311235b4e0a10759e1f7
Levente3D <faylevente@gmail.com>
2020-12-29 04:55:08 +0100

    javaBean commit

---
+++ src/main/java/javaBean/dog.java
@@ -0,0 +1,41 @@
+package javaBean;
+
+public class dog {
+
+    private String name;
+    private int age;
+    private boolean pedigre;
+    private double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isPedigre() {
+        return pedigre;
+    }
+
+    public void setPedigre(boolean pedigre) {
+        this.pedigre = pedigre;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
