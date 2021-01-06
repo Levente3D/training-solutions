421079afb982f3b2bcd251b87312f08d4287abe0
Levente3D <faylevente@gmail.com>
2021-01-06 21:58:54 +0100

    vizsga02_ArraySelector commit

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,22 @@
 package exam02;
 
+import java.lang.reflect.Array;
+
 public class ArraySelector {
+
+    public int getSelectEvens(Array[] parts){
+        StringBuilder result = new StringBuilder();
+
+        if (parts == null){
+            throw new IllegalArgumentException("Üres lista!");
+        }
+        if (parts.lenght() !=0){
+            result.append("[");
+            for(int i =0; i < parts.length(); i+=2){
+                result.append(parts.get[i]);
+            }
+            result.append("]");
+        }
+        return result.toString();
+    }
 }
