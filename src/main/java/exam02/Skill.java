851824e8e761ae1b5f1109bf756312cc2b3d8519
Levente3D <faylevente@gmail.com>
2021-01-06 21:59:48 +0100

    vizsga02_Skill commit

--- src/main/java/exam02/Skill.java
+++ src/main/java/exam02/Skill.java
@@ -1,4 +1,22 @@
 package exam02;
 
 public class Skill {
+    private String name;
+    private int level;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public Skill(String name, int level)
+
+    public Create(String name; int level){
+        return name + "(" + level + ")";
+    }
+
+
 }
