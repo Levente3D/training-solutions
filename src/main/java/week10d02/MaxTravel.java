60200ab053f2850faf9317e70cf741ddea0bf99b
Levente3D <faylevente@gmail.com>
2021-02-20 14:04:30 +0100

    week10day02_commit

---
+++ src/main/java/week10d02/MaxTravel.java
@@ -0,0 +1,19 @@
+package week10d02;
+
+import java.util.Arrays;
+import java.util.List;
+
+ // public class MaxTravel {
+ //   public int getMaxindex(List<Integer> numbers){
+ //       int[] counters = new int[30];
+ //       for (int i: numbers){
+ //           counters[i]++;
+ //       }
+ //       System.out.println(Arrays.toString(counters));
+ //       return int counters;
+ //   }
+
+ //   public static void main(String[] args) {
+ //       new MaxTravel().getMaxindex(List.of(12,12,0,3,4,4,12));
+//   }
+
