73df3fe11dcb195fa4851266827ccd60b8f9bb2d
Levente3D <faylevente@gmail.com>
2021-02-20 13:57:16 +0100

    week06day05_commit

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,26 @@
+package week06d05;
+
+public class Biscuit {
+
+    private BiscuitType type;
+    private int gramAmount;
+
+    public Biscuit(BiscuitType type, int granAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public BiscuitType getType() {
+        return type;
+    }
+
+    public int getGramAmount() {
+        return gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType type, int gramAmount){
+        return new Biscuit(type, gramAmount);
+    }
+
+
+}
